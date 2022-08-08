530f7e09263fd3d8ed7b139a0044b6ce03ef63db
Johannes Gómez <87533958+JohannesGomez@users.noreply.github.com>
2022-08-08 14:26:32 -0400

    Actualizando : disney-world-api

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ import server from './app';
 import { conn } from './dataBase';
 import {addGenders} from './libs/initialSetup'
 
-conn.sync({ force:false}).then(() => { // Syncing all the models at once.
+conn.sync({ force:true}).then(() => { // Syncing all the models at once.
      server.listen(process.env.PORT, () => {
       addGenders();
      console.log('Server listening on Port:', process.env.PORT); 
